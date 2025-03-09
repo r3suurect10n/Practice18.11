22c759f30680596c23d49b06c4cbd2f87d6cd431
r3suurect10n <sosnin.a.a@yandex.ru>
2025-03-09 23:30:12 +0300

    PotionPrefab

---
+++ Assets/HealthPotion.cs
@@ -0,0 +1,17 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class HealthPotion : MonoBehaviour
+{
+    private Coroutine 
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+
+        if (other.GetComponent<Player>())
+        {
+
+        }
+    }
+}
